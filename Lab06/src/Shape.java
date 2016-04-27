a513ded42f4fbea2210c8620114b9d71b075cdaa
윤상철 <@Yoon>
2016-04-27 16:26:38 +0900

    brilliant

--- Lab06/src/Shape.java
+++ Lab06/src/Shape.java
@@ -1,4 +1,20 @@
 
 public abstract class Shape {
+	
+	protected int id;
+	
+	public int getId()
+	{
+		return id;
+	}
+	
+	public void setId(int id)
+	{
+		this.id = id;
+	}
+	
+	public abstract double calculateArea();
+	public abstract double calculatePerimeter();
+	public abstract void printInfo();
 
 }
--- Lab06/src/ShapeTest.java
+++ Lab06/src/ShapeTest.java
@@ -1,6 +1,17 @@
 
 public class ShapeTest {
 
+	
+	public void start()
+	{
+		
+	}
+	
+	public void processShape(Shape s)
+	{
+		
+	}
+	
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 
